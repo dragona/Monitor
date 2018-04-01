ed99bb8dbf76b378d7fb67a46529434049f48ab2
PengfeiMiao <2018224980@qq.com>
2018-04-01 22:18:50 +0800

    describe message

--- app/src/main/java/com/example/a20182/monitor/Application.java
+++ app/src/main/java/com/example/a20182/monitor/Application.java
@@ -1,5 +1,6 @@
 package com.example.a20182.monitor;
 
+import android.content.ComponentName;
 import android.graphics.drawable.Drawable;
 
 /**
@@ -9,18 +10,21 @@ import android.graphics.drawable.Drawable;
 public class Application {
     private String name;
     private Drawable icon;
-    private long runtime;
+    private ComponentName intent;
+    private boolean isRun = false;
+    private int runtime;
+    private int limitime = 0;
+    private String tips = "";
     private boolean isSelected;
-    //private int freq = 0;
-
 
     //无参构造函数
     public Application(){}
 
     //有参构造函数
-    public Application(String name,Drawable icon,long runtime,boolean isSelected){
+    public Application(String name,Drawable icon,ComponentName intent,int runtime,boolean isSelected){
         this.name=name;
         this.icon=icon;
+        this.intent=intent;
         this.runtime=runtime;
         this.isSelected=isSelected;
     }
@@ -41,12 +45,36 @@ public class Application {
         this.icon = icon;
     }
 
-    public long getRuntime() {
+    public ComponentName getIntent () {
+        return intent;
+    }
+
+    public void setIntent (ComponentName intent) {
+        this.intent = intent;
+    }
+
+    public int getLimiTime() {
+        return limitime;
+    }
+
+    public void setLimiTime(int limitime) {
+        this.limitime = limitime;
+    }
+
+    public int getRuntime() {
         return runtime;
     }
 
-    public void setRuntime(long runtime) {
+    public void setRuntime(int runtime) {
         this.runtime = runtime;
+    }
+
+    public String getTips() {
+        return tips;
+    }
+
+    public void setTips(String tips) {
+        this.tips = tips;
     }
 
     public boolean getSelected() {
@@ -55,5 +83,13 @@ public class Application {
 
     public void setSelected(boolean isSelected) {
         this.isSelected = isSelected;
+    }
+
+    public boolean getIsRun() {
+        return isRun;
+    }
+
+    public void setIsRun(boolean isRun) {
+        this.isRun = isRun;
     }
 }
--- app/src/main/java/com/example/a20182/monitor/CustomToast.java
+++ app/src/main/java/com/example/a20182/monitor/CustomToast.java
@@ -1,4 +1,36 @@
 package com.example.a20182.monitor;
 
+import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
 public class CustomToast {
+    private static TextView mTextView;
+    private static ImageView mImageView;
+
+    public static void showToast(Context context, String message) {
+        //load layout
+        View toastRoot = LayoutInflater.from(context).inflate(R.layout.toast, null);
+        //init
+        mTextView = (TextView) toastRoot.findViewById(R.id.message);
+        mImageView = (ImageView) toastRoot.findViewById(R.id.imageView);
+        //settings
+        mTextView.setText(message);
+        mImageView.setImageResource(R.drawable.alert);
+        //Toast init
+        Toast toastStart = new Toast(context);
+        //get screen height
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        int height = wm.getDefaultDisplay().getHeight();
+        //Toast.Y = screen.height*1/3
+        toastStart.setGravity(Gravity.TOP, 0, height / 3);
+        toastStart.setDuration(Toast.LENGTH_LONG);
+        toastStart.setView(toastRoot);
+        toastStart.show();
+    }
 }
