513a81c6c8fe06902c6c61ff92e0f2cb482be629
PengfeiMiao <2018224980@qq.com>
2018-04-02 14:09:08 +0800

    describe message

--- app/src/main/java/com/example/a20182/monitor/Settings.java
+++ app/src/main/java/com/example/a20182/monitor/Settings.java
@@ -4,29 +4,51 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Settings extends AppCompatActivity {
 
+    private static EditText TipsText;
+
     private Application curset = MainActivity.AppList.get(Select.curPosition);
-    private String [] data={"Game Level","Video Level","Music Level","Tools Level"};
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        //创建适配器
+
         TextView appName = (TextView)findViewById(R.id.tv_appName);
         appName.setText(curset.getName()+"  Settings");
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>(
-                Settings.this,android.R.layout.simple_list_item_1, data);
-        ListView listView=(ListView) findViewById(R.id.lv_classify);
-        listView.setAdapter(adapter);
+
+        TipsText = findViewById(R.id.et_content);
+        boolean temp = MainActivity.AppList.get(Select.curPosition).getTips().equals("");
+        TipsText.setText(temp?"Please have a rest!":MainActivity.AppList.get(Select.curPosition).getTips());
+
     }
 
     public void btnConfirm(View view){
-        MainActivity.AppList.get(Select.curPosition).setSelected(true);
-        startActivity(new Intent(Settings.this, MainActivity.class));
+        EditText H_Text = findViewById(R.id.et_limit_h);
+        EditText M_Text = findViewById(R.id.et_limit_m);
+        EditText S_Text = findViewById(R.id.et_limit_s);
+
+        int h = (H_Text.getText().toString().equals(""))?0:Integer.parseInt(H_Text.getText().toString());
+        int m = (M_Text.getText().toString().equals(""))?0:Integer.parseInt(M_Text.getText().toString());
+        int s = (S_Text.getText().toString().equals(""))?0:Integer.parseInt(S_Text.getText().toString());
+
+        int maxtime = Integer.parseInt(H_Text.getText().toString()) * 3600 +
+                Integer.parseInt(M_Text.getText().toString()) * 60 +
+                Integer.parseInt(S_Text.getText().toString());
+
+        if(m>=60 || s>=60 || maxtime==0)Toast.makeText(getApplicationContext(), "Unapt-Input!", Toast.LENGTH_SHORT).show();
+        else {
+            MainActivity.AppList.get(Select.curPosition).setSelected(true);
+            MainActivity.AppList.get(Select.curPosition).setLimiTime(maxtime);
+            MainActivity.AppList.get(Select.curPosition).setTips(TipsText.getText().toString());
+            MainActivity.AppList.get(Select.curPosition).setRuntime(0);
+
+            startActivity(new Intent(Settings.this, MainActivity.class));
+        }
     }
 }
--- app/src/main/java/com/example/a20182/monitor/WindowChangeDetectingService.java
+++ app/src/main/java/com/example/a20182/monitor/WindowChangeDetectingService.java
@@ -1,4 +1,113 @@
 package com.example.a20182.monitor;
 
-public class WindowChangeDetectingService {
+import android.accessibilityservice.AccessibilityService;
+import android.content.ComponentName;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.os.Message;
+
+import android.view.accessibility.AccessibilityEvent;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class WindowChangeDetectingService extends AccessibilityService {
+    public static int position = -1;
+
+    final Timer timer = new Timer();
+    TimerTask task = new TimerTask() {
+        @Override
+        public void run() {
+            if (position != -1) {
+                if (MainActivity.mApps.get(position).getIsRun()) {
+                    MainActivity.mApps.get(position).setRuntime(MainActivity.mApps.get(position).getRuntime() + 1);
+
+                    if(MainActivity.flags_timer&&MainActivity.mApps.get(position).getRuntime()%10==0){handler_timer.sendEmptyMessage(0);}
+                    if (MainActivity.mApps.get(position).getRuntime() >= MainActivity.mApps.get(position).getLimiTime()
+                            && MainActivity.mApps.get(position).getLimiTime() != 0) {
+                        handler.sendEmptyMessage(0);
+                        MainActivity.mApps.get(position).setIsRun(false);
+                        if(MainActivity.flags_auto){MainActivity.mApps.get(position).setRuntime(0);}
+                    }
+                }
+            }
+        }
+    };
+
+
+    @Override
+    protected void onServiceConnected() {
+        super.onServiceConnected();
+        timer.schedule(task, 0, 1000);
+    }
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+
+        if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
+
+            ComponentName componentName = new ComponentName(
+                    event.getPackageName().toString(),
+                    event.getClassName().toString()
+            );
+
+            ActivityInfo activityInfo = tryGetActivity(componentName);
+            boolean isActivity = activityInfo != null;
+            if (isActivity) {
+                for (int i = 0; i < MainActivity.mApps.size(); i++) {
+                    if (componentName.getPackageName().contains("monitor")) {
+                        MainActivity.mApps.get(i).setIsRun(false);
+                    } 
+					else if (MainActivity.mApps.get(i).getIntent().getPackageName().equals(componentName.getPackageName())){
+						MainActivity.mApps.get(i).setIsRun(true);
+						position = i;
+
+                    }else if(i==MainActivity.mApps.size()-1){
+						position = -1;
+					}
+
+                }
+            }
+
+        }
+    }
+
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            CustomToast.showToast(getApplicationContext(), MainActivity.mApps.get(position).getTips());
+        }
+    };
+
+    private Handler handler_timer = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            Toast.makeText(getApplicationContext(), toTime(MainActivity.mApps.get(position).getRuntime()), Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private String toTime(int seconds) {
+
+        int h = seconds/3600;
+        int m = (seconds%3600)/60;
+        int s = (seconds%3600)%60;
+
+        String startDateStr = String.format("%02d",h)+ ":" + String.format("%02d",m) + ":" + String.format("%02d",s);
+
+        return startDateStr;
+    }
+
+    private ActivityInfo tryGetActivity(ComponentName componentName) {
+        try {
+            return getPackageManager().getActivityInfo(componentName, 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            return null;
+        }
+    }
+
+    @Override
+    public void onInterrupt() {}
+
 }
